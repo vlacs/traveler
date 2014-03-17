2d093d6c09cee788a69fd03da1bec477d7ec80c2
Mike George <mikeyg6754@gmail.com>
2014-03-17 17:14:56 -0400

    Move db below system

--- src/clj/traveler/system.clj
+++ src/clj/traveler/system.clj
@@ -10,11 +10,11 @@
 (defn conf []
   (t-conf/load-config))
 
-(def db
-  (d/db @(:db system)))
-
 (def system
   {:web (atom nil) :db (atom nil)})
+
+(def db
+  (d/db @(:db system)))
 
 (defn start-http []
    (reset! (:web system) (run-server t-core/app {:port (get-in (conf) [:web :port])})))
