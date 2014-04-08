f585a7fbc3cf77c215f6888b749dd94cfeeb0030
Mike George <mikeyg6754@gmail.com>
2014-04-08 19:59:32 -0400

    Lay groundwork for api-routes

--- src/clj/traveler/api/routes.clj
+++ src/clj/traveler/api/routes.clj
@@ -1,1 +1,18 @@
-(ns traveler.api.routes)
+(ns traveler.api.routes
+  (:require [liberator.core :refer [resource]]))
+
+;;Need the following routes:
+;;  /api/users/add
+;;  /api/users/search
+;;  /api/users/:id-sk
+
+(def liberator-resources
+  {:users/add    (resource)
+   :users/search (resource)
+   :users/user   (resource)})
+
+(def api-routes
+  [[:any "/api" (str "Traveler API")
+    [:any "/users/add"    (:users/add liberator-resources)]
+    [:any "/users/search" (:users/search liberator-resources)]
+    [:any "/users/:id-sk" (:users/user liberator-resources)]]])
