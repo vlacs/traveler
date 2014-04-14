da2dbbc99d5abeff895d6a84b10b427f86704904
Mike George <mikeyg6754@gmail.com>
2014-04-14 12:31:14 -0400

    validation testing

--- src/clj/traveler/api/users.clj
+++ src/clj/traveler/api/users.clj
@@ -30,5 +30,20 @@
     [:email email-address? "must be a valid email address"]
     [:policies_assent_date present? "must be present"]))
 
+;;testing
+
+;(def user {:id-sk "00392"
+;           :username "mgeorge"
+;           :password "kaj309jdfna0sd9flk4nas2"
+;           :privilege "ADMIN"
+;           :lastname "George"
+;           :firstname "Michael"
+;           :email "mgeorge@vlacs.org"
+;           :policies_assent_date "2001-03-02"
+;           })
+
+
+;;end testing
+
 (defn add-user [ctx]
   (gen-response "This endpoint is not implemented yet!"))
