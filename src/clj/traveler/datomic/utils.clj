fd66ec8d8bcafebcfe2372229ce7b7fccfb88156
Mike George <mikeyg6754@gmail.com>
2014-04-18 11:19:35 -0400

    Temporary datomic connection to avoid cyclic dependency, use hatch, remove ent->db, update documentation

--- src/clj/traveler/datomic/utils.clj
+++ src/clj/traveler/datomic/utils.clj
@@ -1,45 +1,62 @@
 (ns traveler.datomic.utils
   (:require [cheshire.core :refer [generate-string]]
             [datomic.api :as d]
+            [hatch]
+            [traveler.conf :as t-conf]
+            [traveler.schema :as t-schema]
             [traveler.system :as s]
             [traveler.datomic.output-models :refer :all]))
+
+(defn conf []
+  (t-conf/load-config))
+
+(defn db []
+  (d/connect (get-in (conf) [:db :uri])))
+
+(def tx-entity! (partial hatch/tx-clean-entity! t-schema/partitions t-schema/valid-attrs))
 
 (defn find-ents
   "Find an entity using a case-insensitive regex match"
   [attr match]
-  (map #(d/entity (d/db @(:db s/system)) (first %))
+  (map #(d/entity (d/db (db)) (first %))
        (d/q '[:find ?e
               :in $ ?attr ?matcher
               :where
               [?e ?attr ?ln]
               [(re-find ?matcher ?ln)]]
-            (d/db @(:db s/system))
+            (d/db (db))
             attr (re-pattern (str "(?i:.*" match ".*)")))))
 
 (defn ents
   "Return all entities that have specific attribute
-   with option to limit or limit and offset them"
+  with option to limit or limit and offset them"
   ([attr]
-   (map #(d/entity (d/db @(:db s/system)) (first %))
+   (map #(d/entity (d/db (db)) (first %))
         (sort (d/q '[:find ?e
                      :in $ ?a
                      :where [?e ?a]]
-                   (d/db @(:db s/system))
+                   (d/db (db))
                    attr))))
   ([attr limit]
-   (map #(d/entity (d/db @(:db s/system)) (first %))
+   (map #(d/entity (d/db (db)) (first %))
         (take limit (sort (d/q '[:find ?e
                                  :in $ ?a
                                  :where [?e ?a]]
-                               (d/db @(:db s/system))
+                               (d/db (db))
                                attr)))))
   ([attr limit offset]
-   (map #(d/entity (d/db @(:db s/system)) (first %))
+   (map #(d/entity (d/db (db)) (first %))
         (take limit (drop offset (sort (d/q '[:find ?e
                                               :in $ ?a
                                               :where [?e ?a]]
-                                            (d/db @(:db s/system))
+                                            (d/db (db))
                                             attr)))))))
+
+(defn user->db
+  "Take pre-validated user map from liberator request
+  and transact to database"
+  [user]
+  (tx-entity! (db) :user (hatch/slam-all user :user)))
 
 (defn ent->map
   "Convert an entity to a map using an output-model"
@@ -58,7 +75,7 @@
 
 (defn find-ents->json
   "Find entities by attribute and convert to
-   json using an output-model"
+  json using an output-model"
   [attr match output-model]
   (let [entities (find-ents attr match)]
     (map (fn find-ents->json- [e]
@@ -67,8 +84,8 @@
 
 (defn ents->json
   "Convert all entities that have specific attribute
-   to json using an output-model with option to limit
-   or limit and offset them"
+  to json using an output-model with option to limit
+  or limit and offset them"
   ([attr output-model]
    (let [entities (ents attr)]
      (map (fn ents->json- [e]
@@ -86,12 +103,3 @@
      (map (fn ents->json- [e]
             (generate-string (ent->map output-model e)))
           entities))))
-
-(defn ent->db
-  "Adds user map to datomic database"
-  [ent-type ent-map]
-  (let [ent (atom {})]
-    (doseq [[k v] ent-map]
-      (swap! ent assoc (keyword (str ent-type "/" (name k))) v))
-    (swap! ent merge {:db/id (d/tempid :db.part/user)})
-    (d/transact @(:db s/system) (vector @ent))))
