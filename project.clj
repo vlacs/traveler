c49902ddbedd9732cc95cd0c5d32cfe93e88f1b0
Mike George <mikeyg6754@gmail.com>
2014-04-30 14:10:25 -0400

    Bump version add exclusions to deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.vlacs/traveler "0.2.0"
+(defproject org.vlacs/traveler "0.2.3"
   :description "Library that controls storage and management of user data"
   :url "http://vlacs.org"
   :license {:name "Eclipse Public License"
@@ -6,10 +6,11 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.vlacs/hatch "0.1.1" :exclusions [com.datomic/datomic-free]]
-                 [org.vlacs/helmsman "0.1.8"]
-                 [vlacs/timber "0.1.0-SNAPSHOT"]
+                 [org.vlacs/helmsman "0.2.5"]
+                 [org.vlacs/timber "0.1.2" :exclusions [org.vlacs/helmsman]]
                  [cheshire "5.3.1"]
-                 [com.datomic/datomic-free "0.9.4707" :exclusions [commons-codec]]
+                 [com.datomic/datomic-free "0.9.4707"
+                  :exclusions [commons-codec org.jgroups/jgroups org.jboss.logging/jboss-logging]]
                  [crypto-password "0.1.3" :exclusions [commons-codec]]
                  [datomic-schematode "0.1.0-RC1"]
                  [digest "1.4.4"]
@@ -17,6 +18,7 @@
                  [im.chit/gyr "0.3.1"]
                  [inflections "0.9.6"]
                  [liberator "0.10.0" :exclusions [hiccup]]
+                 [org.immutant/immutant "1.1.1"]
                  [ring/ring-core "1.2.2"]
                  [valip "0.2.0"]]
 
