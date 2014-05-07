bcf6215e4d377df2f18bc39cf4d234969248fcc1
Mike George <mikeyg6754@gmail.com>
2014-05-07 12:14:04 -0400

    remove debug output

--- src/clj/traveler/templates.clj
+++ src/clj/traveler/templates.clj
@@ -44,9 +44,6 @@
 (defn nav-side
   "Generate the sidebar"
   [request]
-  (prn (:helmsman request))
-  (prn (h-nav/id->uri-path request :traveler/dashboard))
-  (prn (h-uri/relative-uri-str request (h-nav/id->uri-path request :traveler/dashboard)))
   (timber/main-menu
    [{:menu-name "Dashboard"
      :menu-url  (t-utils/id-uri request :traveler/dashboard)}
